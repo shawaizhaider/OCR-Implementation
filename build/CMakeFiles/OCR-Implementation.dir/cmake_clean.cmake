eee5014b7b5941b22c828498de9fd71aaba4fb80
Shawaiz Haider <shawaizhaider510@gmail.com>
2024-12-16 18:05:58 +0500

    Configured the Directories Commit Summary 	new file:   .vscode/settings.json 	new file:   CMakeLists.txt 	new file:   build/CMakeCache.txt 	new file:   build/CMakeFiles/3.3.2/CMakeCCompiler.cmake 	new file:   build/CMakeFiles/3.3.2/CMakeCXXCompiler.cmake 	new file:   build/CMakeFiles/3.3.2/CMakeDetermineCompilerABI_C.bin 	new file:   build/CMakeFiles/3.3.2/CMakeDetermineCompilerABI_CXX.bin 	new file:   build/CMakeFiles/3.3.2/CMakeSystem.cmake 	new file:   build/CMakeFiles/3.3.2/CompilerIdC/CMakeCCompilerId.c 	new file:   build/CMakeFiles/3.3.2/CompilerIdC/a.out 	new file:   build/CMakeFiles/3.3.2/CompilerIdCXX/CMakeCXXCompilerId.cpp 	new file:   build/CMakeFiles/3.3.2/CompilerIdCXX/a.out 	new file:   build/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   build/CMakeFiles/CMakeOutput.log 	new file:   build/CMakeFiles/Makefile.cmake 	new file:   build/CMakeFiles/Makefile2 	new file:   build/CMakeFiles/OCR-Implementation.dir/CXX.includecache 	new file:   build/CMakeFiles/OCR-Implementation.dir/DependInfo.cmake 	new file:   build/CMakeFiles/OCR-Implementation.dir/build.make 	new file:   build/CMakeFiles/OCR-Implementation.dir/cmake_clean.cmake 	new file:   build/CMakeFiles/OCR-Implementation.dir/depend.internal 	new file:   build/CMakeFiles/OCR-Implementation.dir/depend.make 	new file:   build/CMakeFiles/OCR-Implementation.dir/flags.make 	new file:   build/CMakeFiles/OCR-Implementation.dir/link.txt 	new file:   build/CMakeFiles/OCR-Implementation.dir/progress.make 	new file:   build/CMakeFiles/OCR-Implementation.dir/src/main.cpp.o 	new file:   build/CMakeFiles/TargetDirectories.txt 	new file:   build/CMakeFiles/cmake.check_cache 	new file:   build/CMakeFiles/feature_tests.bin 	new file:   build/CMakeFiles/feature_tests.c 	new file:   build/CMakeFiles/feature_tests.cxx 	new file:   build/CMakeFiles/progress.marks 	new file:   build/Makefile 	new file:   build/OCR-Implementation 	new file:   build/cmake_install.cmake 	new file:   include/neoron.hpp 	modified:   src/main.cpp

---
+++ build/CMakeFiles/OCR-Implementation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OCR-Implementation.dir/src/main.cpp.o"
+  "OCR-Implementation.pdb"
+  "OCR-Implementation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OCR-Implementation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/neoron.hpp
@@ -0,0 +1,21 @@
+#ifndef _NEORON_HPP
+#define _NEORON_HPP
+
+#include <iostream>
+using namespace std;
+
+class Neoron
+{
+    private:
+        double neoronVal;
+        //after passing through activation function
+        double neoronActivation;
+    public:
+        Neoron();
+        ~Neoron();
+
+};
+
+
+
+#endif // _NEORON_HPP
